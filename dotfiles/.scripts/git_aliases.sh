1206590f400d9b3ec302dec2eb9cd7314d2a779f
Frank Di Natale <frank.dinatale1988@gmail.com>
2022-07-24 14:48:28 -0700

    Add git alias script.

---
+++ dotfiles/.scripts/git_aliases.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
+git config --global alias.last 'log -1 HEAD'
